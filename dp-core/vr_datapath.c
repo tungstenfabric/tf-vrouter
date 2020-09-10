1868dff71a6ed8ffb9d980a34503ab3934f74667
hajim <hajim@juniper.net>
2020-09-10 21:54:52 +0530

    MAC-IP learning vRouter crash
    - Fixed the issue by allowing only IPv4 packet for trapping to
      agent.
    Closes-Jira-Bug: CEM-19064
    
    Change-Id: I254d6fc187cb33f38f7a8d1caa019da7c86bbc2f

--- dp-core/vr_datapath.c
+++ dp-core/vr_datapath.c
@@ -681,7 +681,7 @@ vr_virtual_input(unsigned short vrf, struct vr_interface *vif,
                 (pkt->vp_if->vif_flags & VIF_FLAG_L3_ENABLED) &&
                 (pkt->vp_if->vif_flags & VIF_FLAG_L2_ENABLED)){
 
-            if((pkt->vp_type == VP_TYPE_IP) || (pkt->vp_type == VP_TYPE_IP6)) {
+            if(pkt->vp_type == VP_TYPE_IP) {
                 memset(&vr_req, 0, sizeof(struct vr_route_req));
                 rtable = router->vr_inet_rtable;
                 vr_req.rtr_req.rtr_prefix = (uint8_t*)&rt_prefix;
@@ -699,19 +699,19 @@ vr_virtual_input(unsigned short vrf, struct vr_interface *vif,
                     ip6 = (struct vr_ip6 *)(pkt_data(pkt) + sizeof(struct vr_eth));
                     memcpy(vr_req.rtr_req.rtr_prefix, ip6->ip6_src, sizeof(ip6->ip6_src));
                 }
-            }
 
-            rtable->algo_get(fmd->fmd_dvrf, &vr_req);
+                rtable->algo_get(fmd->fmd_dvrf, &vr_req);
 
-            if(vr_req.rtr_nh && vr_req.rtr_nh->nh_id) {
-                if(vr_req.rtr_req.rtr_mac == NULL ||
-                        !(VR_MAC_CMP(eth->eth_smac, vr_req.rtr_req.rtr_mac))) {
-                    vr_trap(pkt, fmd->fmd_dvrf, AGENT_TRAP_MAC_IP_LEARNING, NULL);
-                    return 0;
+                if(vr_req.rtr_nh && vr_req.rtr_nh->nh_id) {
+                    if(vr_req.rtr_req.rtr_mac == NULL ||
+                            !(VR_MAC_CMP(eth->eth_smac, vr_req.rtr_req.rtr_mac))) {
+                        vr_trap(pkt, fmd->fmd_dvrf, AGENT_TRAP_MAC_IP_LEARNING, NULL);
+                        return 0;
+                    }
+                } else {
+                        vr_trap(pkt, fmd->fmd_dvrf, AGENT_TRAP_MAC_IP_LEARNING, NULL);
+                        return 0;
                 }
-            } else {
-                    vr_trap(pkt, fmd->fmd_dvrf, AGENT_TRAP_MAC_IP_LEARNING, NULL);
-                    return 0;
             }
         }
         if ((pkt->vp_if->vif_flags & VIF_FLAG_MAC_IP_LEARNING) &&
