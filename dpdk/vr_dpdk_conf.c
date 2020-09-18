985a5831d24dacf8cf3f1679763f503460ae03be
hajim <hajim@juniper.net>
2020-09-18 16:27:14 +0530

    DDP feature for fortville NIC's
    - By default Intel DDP feature disabled.
    - Provided “--ddp” command line option to enable DDP while loading
      DPDK.
    - Provided API's for Adding/deleting DDP firmware.
    - Added CLI "dpdkinfo --ddp list" to check firmware loaded or not.
    
    Closes-Jira-Bug: CEM-17801
    Change-Id: I1fae54a7aa08773f4646c63b15f501d4515631a1

---
+++ dpdk/vr_dpdk_conf.c
@@ -0,0 +1,55 @@
+/*
+ * vr_dpdk_conf.c - DPDK specific callback functions for dpdkconf CLI.
+ *
+ * Copyright (c) 2020 Juniper Networks, Inc. All rights reserved.
+ */
+
+#include <vr_os.h>
+#include <vr_types.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#include "vr_dpdk.h"
+#include "vrouter.h"
+
+int
+dpdk_conf_add_ddp(VR_INFO_ARGS)
+{
+    VR_INFO_BUF_INIT();
+    int ret;
+
+    ret = vr_dpdk_process_ddp_package(VR_DPDK_DDP_ADD);
+    if(ret == 0) {
+        vr_dpdk_set_ddp();
+        VI_PRINTF("Programming DDP image mplsogreudp - success \n\n");
+        return 0;
+    } else if(ret == -1) {
+        VI_PRINTF("DDP works only on X710 NIC series\n\n");
+    } else {
+        VI_PRINTF("Programming DDP image mplsogreudp -  failed(%d) \n\n", ret);
+    }
+    /* If DDP firmware not programmed on any ports, then dont disable software
+     * load balancing */
+    vr_dpdk_reset_ddp();
+    return 0;
+}
+
+int
+dpdk_conf_del_ddp(VR_INFO_ARGS)
+{
+    VR_INFO_BUF_INIT();
+    int ret;
+
+    ret = vr_dpdk_process_ddp_package(VR_DPDK_DDP_DELETE);
+    if(ret == 0) {
+        vr_dpdk_reset_ddp();
+        VI_PRINTF("Removing DDP image mplsogreudp - success \n\n");
+    } else if(ret == -1) {
+        VI_PRINTF("DDP works only on X710 NIC series\n\n");
+    } else {
+        VI_PRINTF("Removing DDP image mplsogreudp -  failed(%d) \n\n", ret);
+    }
+    return 0;
+}
