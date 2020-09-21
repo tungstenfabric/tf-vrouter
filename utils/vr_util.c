db1c6e141fe69c70d153f7ed6f11858953eec061
Anand Narayanan Rao <anandrao@juniper.net>
2020-09-21 23:28:06 +0530

    L3MH: NH CLI changes to support addition of ECMP NH
    closes-jira-bug: CEM-18440
    Change-Id: I51786fcd7f1243cde5e8fa1c09590b156ac9b48e

--- utils/vr_util.c
+++ utils/vr_util.c
@@ -1895,7 +1895,7 @@ int
 vr_send_nexthop_encap_tunnel_add(struct nl_client *cl, unsigned int router_id,
         unsigned int type, int nh_index, unsigned int flags, int vrf_index,
         int vif_index, int8_t *smac, int8_t *dmac, struct in_addr sip,
-        struct in_addr dip, int sport, int dport, int8_t *l3_vxlan_mac)
+        struct in_addr dip, int sport, int dport, int8_t *l3_vxlan_mac, int family)
 {
     vr_nexthop_req req;
 
@@ -1938,7 +1938,7 @@ vr_send_nexthop_encap_tunnel_add(struct nl_client *cl, unsigned int router_id,
     }
 
     if (type == NH_ENCAP)
-        req.nhr_family = AF_BRIDGE;
+        req.nhr_family = family;
 
     return vr_sendmsg(cl, &req, "vr_nexthop_req");
 }
