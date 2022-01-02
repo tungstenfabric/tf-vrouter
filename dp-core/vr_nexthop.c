4d3bd5128f1e8d35e6b47d9860b4b75d1ccae47e
Ravi Jha <ravikumarj@juniper.net>
2022-01-02 07:45:46 +0000

    21.3.89 - candidate :: rgression :: dpdk-agent restarting after deployment. no triggers.
    Change Description:
    nhr_encap_oif_id gets set only in case of NH_ENCAP, NH_TUNNEL, NH_RCV. For other cases, nhr_encap_oif_id  will possibly be NULL and we don’t require to allocate the memory for this in vr_nexthop_add.
    
    closes-jira-bug: CEM-25396
    Signed-off-by: Ravi Jha <ravikumarj@juniper.net>
    Change-Id: I5ff2b4ea8708f3d8607afe540a42597682d903ea

--- dp-core/vr_nexthop.c
+++ dp-core/vr_nexthop.c
@@ -3941,19 +3941,6 @@ vr_nexthop_add(vr_nexthop_req *req)
     struct vr_nexthop *nh;
     struct vrouter *router = vrouter_get(req->nhr_rid);
 
-    /* nhr_encap_oif_id can be NULL before soft reset,
-     * So, allocating it here with first index as ZERO */
-    if (0 == req->nhr_encap_oif_id_size) {
-        req->nhr_encap_oif_id = vr_zalloc(sizeof(unsigned int),
-                                VR_NEXTHOP_REQ_LIST_OBJECT);
-        if (!req->nhr_encap_oif_id) {
-            ret = -ENOMEM;
-            goto generate_resp;
-        }
-        req->nhr_encap_oif_id_size = 1;
-        req->nhr_encap_oif_id[0] = 0;
-    }
-
     if (!vr_nexthop_valid_request(req) && (ret = -EINVAL))
         goto generate_resp;
 
