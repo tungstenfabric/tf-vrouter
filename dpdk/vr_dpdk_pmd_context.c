2372dc8dd0a582c6803ff04a9f40a23e770613cd
rpennati <roberto.pennati@hcl.com>
2022-02-22 20:25:25 +0100

    [Intel N3000] Refactor PMD context initilization
    Previously it was PMD Context's responsibilityto verify
    if it is enabled and invoke it's logic conditionally.
    That resulted in info logs being shown from the n3k
    context although it was not initialized as the checks
    were done "internally".
    
    Better design is to add another callback through which
    we can verify if the PMD Context is enabled or not and
    eventually call it's initialization logic.
    
    Partial-jira-bug: TFF-24
    Signed-off-by: Roberto Pennati <roberto.pennati@hcl.com>
    Change-Id: I27023c69b0a4944ae838bde9d277c3d79917f185

--- dpdk/vr_dpdk_pmd_context.c
+++ dpdk/vr_dpdk_pmd_context.c
@@ -32,11 +32,11 @@ vr_dpdk_pmd_ctx_print_usage(void)
 {
     struct vr_dpdk_pmd_ctx *ctx = pmd_ctx;
 
-    if (!ctx || !ctx->print_usage) {
-        return -ENOTSUP;
+    if (ctx != NULL && ctx->print_usage != NULL) {
+        return ctx->print_usage();
     }
 
-    return ctx->print_usage();
+    return -ENOTSUP;
 }
 
 int
@@ -44,57 +44,73 @@ vr_dpdk_pmd_ctx_parse_opt(int vr_argc, char *vr_argv[], size_t optindex, char op
 {
     struct vr_dpdk_pmd_ctx *ctx = pmd_ctx;
 
-    if (!ctx || !ctx->parse_opt) {
-        return -ENOTSUP;
+    if (ctx != NULL && ctx->parse_opt != NULL) {
+        return ctx->parse_opt(vr_argc, vr_argv, optindex, opt, optarg);
     }
 
-    return ctx->parse_opt(vr_argc, vr_argv, optindex, opt, optarg);
+    return -ENOTSUP;
 }
 
 int
 vr_dpdk_pmd_ctx_init(int eal_argc, char *eal_argv[])
 {
     struct vr_dpdk_pmd_ctx *ctx = pmd_ctx;
+    bool init_callable = ctx != NULL &&
+                         ctx->is_enabled != NULL &&
+                         ctx->is_enabled() &&
+                         ctx->init != NULL;
 
-    if (!ctx || !ctx->init) {
-        return rte_eal_init(eal_argc, eal_argv);
+    if (init_callable) {
+        return ctx->init(eal_argc, eal_argv);
     }
 
-    return ctx->init(eal_argc, eal_argv);
+    return rte_eal_init(eal_argc, eal_argv);
 }
 
 int
 vr_dpdk_pmd_ctx_exit(void)
 {
     struct vr_dpdk_pmd_ctx *ctx = pmd_ctx;
+    bool exit_callable = ctx != NULL &&
+                         ctx->is_enabled != NULL &&
+                         ctx->is_enabled() &&
+                         ctx->exit != NULL;
 
-    if (!ctx || !ctx->exit) {
-        return rte_eal_cleanup();
+    if (exit_callable) {
+        return ctx->exit();
     }
 
-    return ctx->exit();
+    return rte_eal_cleanup();
 }
 
 int
 vr_dpdk_pmd_ctx_lcore_request(char *lcores_string, size_t lcores_string_sz, char *service_core_mapping)
 {
     struct vr_dpdk_pmd_ctx *ctx = pmd_ctx;
+    bool request_callable = ctx != NULL &&
+                            ctx->is_enabled != NULL &&
+                            ctx->is_enabled() &&
+                            ctx->lcore_request != NULL;
 
-    if (!ctx || !ctx->lcore_request) {
-        return -ENOTSUP;
+    if (request_callable) {
+        return ctx->lcore_request(lcores_string, lcores_string_sz, service_core_mapping);
     }
 
-    return ctx->lcore_request(lcores_string, lcores_string_sz, service_core_mapping);
+    return -ENOTSUP;
 }
 
 int
 vr_dpdk_pmd_ctx_launch_lcores(vr_dpdk_lcore_launch_cb_t launch_cb)
 {
     struct vr_dpdk_pmd_ctx *ctx = pmd_ctx;
+    bool launch_callable = ctx != NULL &&
+                           ctx->is_enabled != NULL &&
+                           ctx->is_enabled() &&
+                           ctx->launch_lcores != NULL;
 
-    if (!ctx || !ctx->launch_lcores) {
-        return rte_eal_mp_remote_launch(launch_cb, NULL, CALL_MASTER);
+    if (launch_callable) {
+        return ctx->launch_lcores(launch_cb);
     }
 
-    return ctx->launch_lcores(launch_cb);
+    return rte_eal_mp_remote_launch(launch_cb, NULL, CALL_MASTER);
 }
